985c3e7431ce0347af50f6e132f1fd2731b58267
Jeremiah Nwaeze <jerry@karixchange.com>
2018-11-23 12:06:57 +0100

    Second Heroku commit

---
+++ public/build/precache-manifest.0188386b8a2c37a22ca65c92b1e53008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/spaceX.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "572e20048ef00ef86252",
+    "url": "/static/js/main.572e2004.chunk.js"
+  },
+  {
+    "revision": "d750676e5dd8631213b7",
+    "url": "/static/js/1.d750676e.chunk.js"
+  },
+  {
+    "revision": "572e20048ef00ef86252",
+    "url": "/static/css/main.3bb90ff3.chunk.css"
+  },
+  {
+    "revision": "da6e4aced4ee73ed3d615ca9ef4e2186",
+    "url": "/index.html"
+  }
+];
