76123aff1b790efc921d3a86c0fd9255edb8f3b5
Jeremiah Nwaeze <jerry@karixchange.com>
2018-11-23 12:01:36 +0100

    First Heroku Commit

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema')
-// const express = require('graphql');
+const cors = require('cors');
+const schema = require('./schema');
+const path = require('path');
 
 const app = express();
+
+// Allow cross-origin
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+app.use(express.static('public'));
+
+app.get('*', ()=> {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
 
 const PORT = process.env.PORT || 5000;
 
